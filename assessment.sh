2abefdad50fc1c627b794e3c88f0655ec47473fe
root <root@Loaner01.providencepubliclibrary.org>
2023-05-09 10:19:13 -0400

    initial commit

---
+++ assessment.sh
@@ -0,0 +1,18 @@
+string=sudo cat /etc/passwd
+echo "${string}" | md5sum >> /var/log/current_users
+old = $(cat /var/log/current_users)
+new = $(cat /etc/passwd|md5sum)
+    if [ $new != $old ]; then
+        echo "$(date + '%D %T') changes occured" >> /var/log/user_changes
+        $new|md5sum > /var/log/current_users
+    fi
+
+crontab -e
+
+60 * * * * root /home/raeoxfordhebron/technical-assessment/assessment.sh
+
+
+
+
+
+
