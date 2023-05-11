1b3c2cba0a353076de284ae4adcc69cb6338d571
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-05-11 14:04:35 -0400

    manipulate user directories

--- assessment.sh
+++ assessment.sh
@@ -1,15 +1,15 @@
-string=sudo cat /etc/passwd
+string=sudo cat /etc/passwd | cut -d " " -f1| sort | uniq -c | sort -rn | head
 echo "${string}" | md5sum >> /var/log/current_users
-old = $(cat /var/log/current_users)
-new = $(cat /etc/passwd|md5sum)
-    if [ $new != $old ]; then
+old=$(cat /var/log/current_users)
+new=$(cat /etc/passwd|md5sum)
+    if [$new!=$old]; then
         echo "$(date + '%D %T') changes occured" >> /var/log/user_changes
         $new|md5sum > /var/log/current_users
     fi
 
-crontab -e
+# crontab -e
 
-60 * * * * root /home/raeoxfordhebron/technical-assessment/assessment.sh
+# 60 * * * * root /home/raeoxfordhebron/technical-assessment/assessment.sh
 
 
 
